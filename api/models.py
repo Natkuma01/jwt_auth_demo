37a49596ac8217fce58c14dfb892b758a9d48035
Shimin <natkuma@outloook.com>
2024-03-18 22:54:28 -0400

    first commit

---
+++ api/models.py
@@ -0,0 +1,11 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+
+# # Create your models here.
+# class User(AbstractUser):
+#     username = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = ["username"]
